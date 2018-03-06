28b749278463f3c387c4564b3060075ff7c4a4fa
polinafiksson <pd.fiksson@gmail.com>
2018-03-06 11:04:02 -0600

    Initial Commit

---
+++ Mooskine/Model/DataController.swift
@@ -0,0 +1,14 @@
+//
+//  DataController.swift
+//  Mooskine
+//
+//  Created by Polina Fiksson on 06/03/2018.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    
+}
