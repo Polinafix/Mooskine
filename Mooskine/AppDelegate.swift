305da5153545313e5d28e6f6f62fd6f4d40e4a48
polinafiksson <pd.fiksson@gmail.com>
2018-03-08 12:01:51 -0600

    saving the context in the appDelegate methods

--- Mooskine/AppDelegate.swift
+++ Mooskine/AppDelegate.swift
@@ -40,6 +40,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        saveViewContext()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -52,6 +53,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        saveViewContext()
+    }
+    //16. helper method
+    func saveViewContext() {
+        try? dataController.viewContext.save()
     }
 
 
